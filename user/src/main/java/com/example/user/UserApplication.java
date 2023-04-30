b7e269f16693ebfc219d93b7e72f3bd26d130c18
sonaliraj10 <48977626+sonaliraj10@users.noreply.github.com>
2023-05-01 01:10:59 +0530

    Eureka client

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableFeignClients
 @ComponentScan({"com.example.user.controller","com.example.user.service"})
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class UserApplication {
 
 	public static void main(String[] args) {
